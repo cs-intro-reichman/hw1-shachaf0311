7186a785779d2774784284af99c41f6b7923b7fd
shachaf kuyumgiski <shachaf.kuyumgiski@post.runi.ac.il>
2025-10-29 20:52:24 +0200

    fisrt try

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,15 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
+		String name2 = args[1];
+		String name3 = args[2];
+		int bill = Integer.parseInt(args[3]); 
+		double NumOfDiners = 3.0;
+		double SumPerDiner = (double)bill/NumOfDiners;
+		double Pay = Math.ceil(SumPerDiner);
+		System.out.println("Dear " + name3 + ", " + name2 + " and " + name1 +
+							": pay" + Pay + " Shekels each");
+
 	    // Replace this comment with the rest of your code   
 	}
 }
