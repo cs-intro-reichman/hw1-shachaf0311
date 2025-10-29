3ac304ad3cf5bf52d8b9990db50581e92a7e75f8
shachaf kuyumgiski <shachaf.kuyumgiski@post.runi.ac.il>
2025-10-29 21:18:45 +0200

    third at

--- Bill3.java
+++ Bill3.java
@@ -9,7 +9,7 @@ public class Bill3 {
 		double NumOfDiners = 3.0;
 		double SumPerDiner = (double)bill/NumOfDiners;
 		double Pay = Math.ceil(SumPerDiner);
-		System.out.println("Dear " + name3 + ", " + name2 + " and " + name1 +
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 +
 							": pay " + Pay + " Shekels each");
 
 	    // Replace this comment with the rest of your code   
