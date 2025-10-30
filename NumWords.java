6a82a512303b192e016bf3f04ef62716adb8ae3e
shachaf kuyumgiski <shachaf.kuyumgiski@post.runi.ac.il>
2025-10-30 11:34:35 +0200

    second

--- NumWords.java
+++ NumWords.java
@@ -5,6 +5,6 @@ public class NumWords {
 	    int hundreds = num / 100;
 	    int tens = (num % 100) / 10;
 	    int units = num % 10;
-		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + units + " ones");	
+		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + units + " ones.");	
 	}
 }
