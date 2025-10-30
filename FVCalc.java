fdafeffcf57053eeb6cf4493120d6f7399e4a8ca
shachaf kuyumgiski <shachaf.kuyumgiski@post.runi.ac.il>
2025-10-30 11:18:07 +0200

    second

--- FVCalc.java
+++ FVCalc.java
@@ -2,12 +2,12 @@
 public class FVCalc {
 	public static void main(String[] args){
 		int sum = Integer.parseInt(args[0]);
-		int interest = Integer.parseInt(args[1]);
+		double interest = Double.parseDouble(args[1]);
 		int years = Integer.parseInt(args[2]);
-		double rate = (double)interest/100;
+		double rate = interest/100;
 		double FV = sum * Math.pow((1+ rate), years);
 		System.out.println("After " + years + " years, $" + sum +
-		" saved at " + (double)interest + "% will yield $" + (int)FV);
+		" saved at " + interest + "% will yield $" + (int)FV);
 
 
 
